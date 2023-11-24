a2fa424e4a25ef95d4d538052905b6d7ef0dd41d
yogesh <yogeshgawali141@gmail.com>
2023-11-24 22:23:54 +0530

    Yogesh 2 Commit.

--- ProductManagmentSystem/src/main/java/com/product/runner/MainProductManagmentSystem.java
+++ ProductManagmentSystem/src/main/java/com/product/runner/MainProductManagmentSystem.java
@@ -1,9 +1,12 @@
 package com.product.runner;
 
+import java.sql.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.product.entity.User;
 import com.product.entity.UserRole;
 import com.product.serviceImpl.UserRoleServiceImpl;
 import com.product.serviceImpl.UserServiceImpl;
@@ -17,6 +20,9 @@ public class MainProductManagmentSystem implements CommandLineRunner {
 	@Autowired
 	private UserRoleServiceImpl userroleservice;
 	
+	
+	private Date date ;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -25,12 +31,43 @@ public class MainProductManagmentSystem implements CommandLineRunner {
 			
 			//register role
 			
-			UserRole role1 = new UserRole(null,"seller","I'm seller");
 			
-			userroleservice.saveUserRole(role1);
+			  UserRole role1 = new UserRole(null,"seller","I'm seller");
+			  
+			  userroleservice.saveUserRole(role1);
+			 
+			
+			  //register user
+			  
+			   User user1 = new User();
+			   
+			   date = new Date(14, 1, 2000);
+			   
+			   user1.setAddress("Pune");
+			   user1.setBalance(1000.00);
+			   
+			   user1.setDob(date);
+			   
+			   user1.setEmailId("yogeshgawali141@gmail.com");
+			   
+			   user1.setFirstName("Yogesh");
+			   
+			   user1.setLastName("Gawali");
+			   
+			   user1.setMobileNo("9130976605");
+			   
+			   user1.setPassword("Spark@123");
+			   
+			   user1.setUserId(null);
+			   
+			   user1.setUserrole(role1);
+			   
+			   userservice.saveUser(user1);
+			   
 			
 			
 			
+		
 			
 			
 			
